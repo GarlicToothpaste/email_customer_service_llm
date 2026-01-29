8389f3006fb5a7f751c4235e1589a56a55035a80
GarlicToothpaste <adrianpdanao@gmail.com>
2026-01-29 20:44:02 +0800

    Initial function for get documentation

--- utils/tools.py
+++ utils/tools.py
@@ -1,10 +1,13 @@
 from rag_functions import initialize_llm, initialize_vectorstore, query_rag
 
+llm = initialize_llm()
+vectorstore = initialize_vectorstore()
+
 def send_email():
     print ("Test")
 
-def get_documentation():
-    print("Test")
+def get_documentation(question: str):
+    query_rag(question=question, vectorstore=vectorstore,llm=llm)
 
 def create_ticket():
     print("Test")
