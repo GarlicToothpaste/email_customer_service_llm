8f466951f946b19772b06050713522d95ebb1344
GarlicToothpaste <adrianpdanao@gmail.com>
2026-02-01 19:06:52 +0800

    Created the code for connecting to the database

--- utils/tools.py
+++ utils/tools.py
@@ -1,8 +1,43 @@
 from utils.rag_functions import initialize_llm, initialize_vectorstore, query_rag
 # from langchain.tools import tool
 
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from sqlalchemy.engine import URL
+from sqlalchemy import create_engine, text
+
+
+load_dotenv()  
+
+db_type = os.environ.get("DB_TYPE") 
+driver = os.environ.get("DB_DRIVER")
+username = os.environ.get("DB_USERNAME")
+password = os.environ.get("DB_PASSWORD")
+host = os.environ.get("DB_HOST")
+database = os.environ.get("DATABASE")
+port = os.environ.get("PORT") # Optional, defaults are often fine
+
+if driver:
+    drivername = f"{db_type}+{driver}"
+else:
+    drivername = db_type
+
+DATABASE_URL = URL.create(
+    drivername=drivername,
+    username=username,
+    password=password,
+    host=host,
+    database=database,
+    port=port,
+)
+
+engine = create_engine(DATABASE_URL, echo=True) 
+
+
 llm = initialize_llm()
 vectorstore = initialize_vectorstore()
+
 
 def send_email():
     print ("Test")
