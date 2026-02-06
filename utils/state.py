6a8e35425ecf401e223d7a9e0f5103e93d2ee739
GarlicToothpaste <adrianpdanao@gmail.com>
2026-02-06 20:00:22 +0800

    Added state for ticket classification

--- utils/state.py
+++ utils/state.py
@@ -6,12 +6,16 @@ class EmailClassification(TypedDict):
     topic : str 
     summary : str 
 
+class TicketClassification(TypedDict):
+    intent: Literal['create_ticket', 'retrieve_ticket']
+
 class EmailAgentState(TypedDict):
     email_content: str
     sender_email:str
     email_id : str 
 
     classification : EmailClassification | None 
+    ticket_classification: TicketClassification | None
 
     search_results : list[str] | None
     customer_history : dict | None 
